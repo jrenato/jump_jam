66463be1d8c936672f3a194ea86d114a936ac040
karvalho <renato@karvalho.com.br>
2023-12-23 15:42:24 -0300

    Added dynamic ground generation

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,17 +2,23 @@ extends Node2D
 
 @export var camera_scene: PackedScene
 @export var platform_scene: PackedScene
+@export var ground_y_offset: int = 62
 
 var camera: GameCamera
+var viewport_size: Vector2
 
 @onready var player: Player = %Player
 @onready var platforms_parent: Node2D = %PlatformsParent
 
 
 func _ready() -> void:
+	viewport_size = get_viewport_rect().size
+
 	camera = camera_scene.instantiate() as GameCamera
 	camera.setup_camera(player)
 	add_child(camera)
+
+	create_ground_platforms()
 
 
 func _process(delta: float) -> void:
@@ -30,3 +36,12 @@ func create_platform(location: Vector2) -> Platform:
 	platform.global_position = location
 	platforms_parent.add_child(platform)
 	return platform
+
+
+func create_ground_platforms() -> void:
+	var platform_width: int = 136
+	var ground_platform_count: int = floor(viewport_size.x / platform_width) + 1
+
+	for i in ground_platform_count:
+		var ground_location: Vector2 = Vector2(platform_width * i, viewport_size.y - ground_y_offset)
+		create_platform(ground_location)
