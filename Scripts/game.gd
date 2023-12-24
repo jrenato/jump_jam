576ada3e49e5e632dbd22f809c1780000f6307a4
karvalho <renato@karvalho.com.br>
2023-12-24 17:10:39 -0300

    Added ground sprite

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -9,12 +9,16 @@ var player: Player
 var player_spawn_position: Vector2
 
 @onready var level_generator: LevelGenerator = %LevelGenerator
+@onready var ground_sprite: Sprite2D = %GroundSprite
 
 
 func _ready() -> void:
 	var viewport_size: Vector2 = get_viewport_rect().size
 	player_spawn_position.x = viewport_size.x / 2.0
 	player_spawn_position.y = viewport_size.y - player_spawn_y_offset
+
+	ground_sprite.global_position.x = viewport_size.x / 2.0
+	ground_sprite.global_position.y = viewport_size.y
 
 	new_game()
 
