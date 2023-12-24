00cc8d06f955a46dab7e506419089e8168644c14
karvalho <renato@karvalho.com.br>
2023-12-24 16:27:13 -0300

    Small clean up

--- Scripts/platform.gd
+++ Scripts/platform.gd
@@ -5,10 +5,6 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		var player: Player = body as Player
